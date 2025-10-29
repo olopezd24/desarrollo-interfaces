9d790d5f6fb124b9b43c2f238d1bc451234aa76b
Oscar Lopez <olopezd24@fpcoruna.afundacion.org>
2025-10-29 09:14:05 +0100

    Añade operaciones.py

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    return a + b
+def resta(a, b):
+    return a - b
+def multiplicacion(a, b):
+    return a * b
+def division(a, b):
+    if b == 0:
+        return "Error: división por cero"
+    return a / b
