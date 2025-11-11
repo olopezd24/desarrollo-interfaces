49b93289c962606f2face7eb161cc972bff8c055
oscarlopezdli <oscarlopezdli@gmail.com>
2025-11-11 21:03:14 +0100

    Añadido DetailActivity con botón en CatalogActivity

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,7 +1,8 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CatalogActivity extends AppCompatActivity {
@@ -9,6 +10,12 @@ public class CatalogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catalog);
+
+        Button Button = findViewById(R.id.btn_navegar_detalle);
+        Button.setOnClickListener(v -> {
+            Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.mycatalog;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_activity);
+    }
+}
